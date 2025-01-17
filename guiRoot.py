c14ae6d148acfaddf7891e12bb9cc983a87718c0
Alvin Chung <alvinchung@vt.edu>
2025-01-17 02:40:59 -0500

    Added some GUI elements without functionality

--- guiRoot.py
+++ guiRoot.py
@@ -6,6 +6,7 @@ Description: The root tkinter object for the GUI application
 '''
 
 import tkinter as tk
+import globalVariables as globals
 
 class GuiRoot(tk.Tk):
     def __init__(self):
@@ -14,8 +15,28 @@ class GuiRoot(tk.Tk):
         self.resizable(False, False)
         
         #Title of the window
-        self.title("S25-38") #TODO - Provide suitable window title
+        self.title("S25-38") #TODO - Provide suitable title
+        self.geometry(globals.GUI_WINDOW_SIZE)
 
-        self.testLabel = tk.Label(text="TEST")
+        self.testLabel = tk.Label(self, text="S25-38 Machine Instruction Converter")
+        self.testLabel.pack(anchor="center")
 
-        self.testLabel.pack()
+        #Import Button
+        self.importFileButton = tk.Button(self, text="Import File")
+        self.importFileButton.pack(anchor="w", padx=5, pady=5)
+
+        #Set Export Destination Button
+        self.exportFileButton = tk.Button(self, text="Set Export Destination")
+        self.exportFileButton.pack(anchor="w", padx=5, pady=5)
+
+        #Conversion Settings Button
+        self.conversionSettings = tk.Button(self, text="Conversion Settings")
+        self.conversionSettings.pack(anchor="w", padx=5, pady=5)
+
+        #Start Conversion Process Button
+        self.startConvButton = tk.Button(self, text="Start Conversion")
+        self.startConvButton.pack(anchor="center", padx=5, pady=5)
+
+        #Status Text Area
+        self.statusTextArea = tk.Text(self, wrap=tk.WORD)
+        self.statusTextArea.pack(side="bottom", padx=5, pady=5)
