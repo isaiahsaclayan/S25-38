c743a0282e9da6f81de49c39dc9b8acee46b93d3
Isaiah Amir Saclayan <isaiahsaclayan@vt.edu>
2025-01-28 18:09:39 -0500

    added framework for converter classes

--- source/transpiler/nscryptConverter.py
+++ source/transpiler/nscryptConverter.py
@@ -1,4 +1,28 @@
 from . import ToolpathConverter
+from typing import List
+
+COMMAND_MAP: dict[str,str] = {
+    "GenericCommand" : "nScrypt Command"
+}
 
 class NscryptConverter(ToolpathConverter):
-    pass
+    def __init__(self):
+        super().__init__(COMMAND_MAP)
+
+    def _get_command(self, command: dict[str, dict[str, str]]) -> str:
+        """
+        Performs translation of a single command
+        :param command: individual command to be translated
+        :return: string representation of the translated command
+        """
+        # TODO: perform translation of single command
+        pass
+
+    def translate(self, parsed_commands: List[dict[str, dict[str, str]]]) -> List[str]:
+        """
+        Translates generic toolpath to list of formatted commands
+        :param parsed_commands: list of commands to be translated from generic parser
+        :return: list of strings that are translated commands
+        """
+        # TODO: perform translation of list of commands
+        pass
--- source/transpiler/toolpathConverter.py
+++ source/transpiler/toolpathConverter.py
@@ -3,14 +3,18 @@ from typing import List
 # Parent Class for Generic to Language Specific Conversion
 class ToolpathConverter:
 
-    # Initializer
-    def __init__(self):
+    def __init__(self, command_map: dict[str,str]):
+        """
+        :param command_map: Dictionary of generic command to language specific command
+        """
+
         # Stores the mapping from the generic command to desired machine's command
-        self.commandMap: dict[str, str] = {}
+        self._command_map: dict[str, str] = command_map
 
     def _get_command(self, command: dict[str, dict[str,str]]) -> str:
         """
         Performs translation of a single command
+        To be overwritten in inherited classes
         :param command: individual command to be translated
         :return: string representation of the translated command
         """
@@ -19,6 +23,7 @@ class ToolpathConverter:
     def translate(self, parsed_commands: List[dict[str, dict[str, str]]]) -> List[str]:
         """
         Translates generic toolpath to list of formatted commands
+        To be overwritten in inherited classes
         :param parsed_commands: list of commands to be translated from generic parser
         :return: list of strings that are translated commands
         """
