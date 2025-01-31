e833cde75d9b5cfca35d7524894571f33a6db499
Alvin Chung <alvinchung@vt.edu>
2025-01-31 16:57:56 -0500

    Added tests for remaining buttons

--- tests/testGui.py
+++ tests/testGui.py
@@ -16,6 +16,30 @@ class TestGuiButtons(unittest.TestCase):
         self.guiRootObj.importFileButton.invoke()
         
         importMock.assert_called()
+    
+    def testExport(self):
+        exportMock = mock.Mock()
 
+        self.guiRootObj.exportFileButton.configure(command = exportMock)
+        self.guiRootObj.exportFileButton.invoke()
+        
+        exportMock.assert_called()
+
+    def testConvSettings(self):
+        convSettingsMock = mock.Mock()
+
+        self.guiRootObj.conversionSettings.configure(command = convSettingsMock)
+        self.guiRootObj.conversionSettings.invoke()
+        
+        convSettingsMock.assert_called()
+
+    def testStartConv(self):
+        startConvMock = mock.Mock()
+
+        self.guiRootObj.startConvButton.configure(command = startConvMock)
+        self.guiRootObj.startConvButton.invoke()
+        
+        startConvMock.assert_called()
+    
 if __name__ == "__main__":
     unittest.main()
