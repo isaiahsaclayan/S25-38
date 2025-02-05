6cdc9c1f89936f1446ba3544127862047f7abd90
Isaiah Amir Saclayan <isaiahsaclayan@vt.edu>
2025-02-05 14:08:05 -0500

    added framework for testing acspl converter

--- tests/test_acsplConverter.py
+++ tests/test_acsplConverter.py
@@ -1,1 +1,34 @@
-import pytest
+import unittest
+import sys
+sys.path.append("../source/transpiler/")
+from acsplConverter import AcsplConverter
+
+MOVE_COMMAND = {
+        "MOVE": {
+            "X": "10",
+            "Y": "20",
+            "Z": "30",
+            "A": "40",
+            "B": "50"
+        }
+    }
+
+INVALID_COMMAND = {
+        "INVALID": {
+            "X": "10",
+            "Y": "20",
+            "Z": "30",
+            "A": "40",
+            "B": "50"
+        }
+    }
+
+class TestAcsplConverter(unittest.TestCase):
+    def setUp(self):
+        self.acsplConverter = AcsplConverter()
+
+    def test_constructor(self):
+        self.assertIsNotNone(self.acsplConverter)
+
+if __name__ == "__main__":
+    unittest.main()
