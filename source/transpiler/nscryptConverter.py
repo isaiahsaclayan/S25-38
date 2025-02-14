d17e5ca21b8ef732ea7e2ceabd4cc608c534401e
Andrew Viola <aviola@vt.edu>
2025-02-14 18:51:48 -0500

    Wrote out most conversions and tests

--- source/transpiler/nscryptConverter.py
+++ source/transpiler/nscryptConverter.py
@@ -7,10 +7,14 @@ Description: Performs generic toolpath to nScrypt conversion.
 
 # Imports
 from toolpathConverter import ToolpathConverter # Parent Class
+from applicationGlobals import writeStatusQueue
 from typing import List
 
 TOOL = "spindle_speed"
 INVALID_COMMAND = "INVALID"
+DO_NOT_SHOW = "DO_NOT_SHOW"
+VERSION = "Version 1.1"
+TYPE = "Type Vector"
 
 SUPPORTED_COMMANDS: List[str] = [
     "move",
@@ -18,27 +22,85 @@ SUPPORTED_COMMANDS: List[str] = [
     "units"
 ]
 
+SUPPORTED_UNITS: List[str] = [
+    "MILLIMETERS",
+    "INCHES"
+]
+
 class NscryptConverter(ToolpathConverter):
     def __init__(self):
         super().__init__(SUPPORTED_COMMANDS)
-        self._units = None
+        self._units = SUPPORTED_UNITS[0] # Millimeters
 
     def _process_command(self, command: str, params: dict[str, str]):
         """
         Processes a single command
         :param command: individual command to be translated
         """
-        # TODO: perform processing of single command
         if command not in self._supported_commands:
+            writeStatusQueue(f"Invalid command: {command}")
             return INVALID_COMMAND
         else:
-            if command == SUPPORTED_COMMANDS[0]:
-                pass
-            elif command == SUPPORTED_COMMANDS[1]:
-                pass
-            elif command == SUPPORTED_COMMANDS[2]:
-                pass
-        pass
+            if command == SUPPORTED_COMMANDS[0]: # Move
+                converted_command = self._translate_move(params)
+            elif command == SUPPORTED_COMMANDS[1]: # Tool on/off command
+                converted_command = self._tool_on_off(params)
+            elif command == SUPPORTED_COMMANDS[2]: # Units
+                converted_command = self._change_units(params)
+            return converted_command
+    
+    def _translate_move(self, params: dict[str, str]):
+        """
+        Translate move command to nScrypt format
+        :param params: dictionary of move command parameters
+        """
+        conversion_factor = 1
+        if self._units == SUPPORTED_UNITS[1]: # Inches
+            conversion_factor = 25.4
+        elif self._units == SUPPORTED_UNITS[0]: # Millimeters
+            conversion_factor = 1
+        # nScrypt will always do XYZ X'Y'Z' or XYZ 0W
+        converted_command = f"{float(params['x'])*conversion_factor} {float(params['y'])*conversion_factor} {float(params['z'])*conversion_factor}"
+        params.pop('x')
+        params.pop('y')
+        params.pop('z')
+        # Not sure what the remaining axis would be labeled from Creo, so I just append all remaining axis
+        remaining_axis = list(params.keys())
+        append0 = 2
+        if len(remaining_axis) == 1:
+            append0 = 1
+        elif len(remaining_axis) == 2:
+            append0 = 0
+        remaining_axis.sort()
+        for axis in remaining_axis:
+            converted_command += f" {float(params[axis])*conversion_factor}"
+        for i in range(append0):
+            converted_command += " 0.0"
+        return converted_command
+                
+    
+    def _change_units(self, params: dict[str, str]):
+        """
+        Change units for nScrypt
+        :param params: dictionary of units command parameters
+        """
+        if params["units"] not in SUPPORTED_UNITS:
+            writeStatusQueue(f"Invalid units: {params['units']}")
+            return INVALID_COMMAND
+        else:
+            self._units = params["units"]
+            return DO_NOT_SHOW
+        
+    def _tool_on_off(self, params: dict[str, str]):
+        """
+        Translate tool on/off command to nScrypt format
+        :param params: dictionary of tool on/off command parameters
+        """
+        if params["control"][:3] == "OFF":
+            return "TOOL OFF"
+        else:
+            return "TOOL ON"
+        
 
     def translate(self, parsed_commands: List[dict[str, dict[str, str]]]) -> List[str]:
         """
@@ -46,21 +108,17 @@ class NscryptConverter(ToolpathConverter):
         :param parsed_commands: list of commands to be translated from generic parser
         :return: list of strings that are translated commands
         """
-        # TODO: perform translation of list of commands
         nScrypt_commands = []
+        nScrypt_commands.append("Version 1.1")
+        nScrypt_commands.append("Type Vector") # TODO: Eventually determine between Vector and Spherical, for now just Vector
         for command_info in parsed_commands:
             command = list(command_info.keys())[0]
             params = command_info[command]
             converted_command = self._process_command(command, params)
-            if converted_command != INVALID_COMMAND:
+            if converted_command != INVALID_COMMAND and converted_command != DO_NOT_SHOW:
                 nScrypt_commands.append(converted_command)
                 self._translated_commands.append(converted_command)
+            elif converted_command == INVALID_COMMAND:
+                nScrypt_commands.append(f"!INVALID COMMAND: {command_info}")
+            
         return nScrypt_commands
-            
-            
-test1 = ToolpathConverter(SUPPORTED_COMMANDS)
-test2 = NscryptConverter()
-
-commands = [{"move":{"x":"1","y":"2","z":"3"}}]
-
-test2.translate(commands)
--- tests/test_nscryptConverter.py
+++ tests/test_nscryptConverter.py
@@ -1,1 +1,159 @@
-import pytest
+import unittest
+import sys
+sys.path.append("../source/transpiler/")
+from nscryptConverter import NscryptConverter, VERSION, TYPE
+
+PARSED_COMMANDS = [
+    {'title': {'type': 'Pro/CLfile', 'version': '11.0 - 11.0.0.0'}},
+    {'manufacturer_number': {'manufacturer_number': 'MFG0006'}},
+    {'part_number': {'part_number': 'MFG0006'}},
+    {'feature_number': {'feature_number': '402'}},
+    {'machine_info': {'machine_type': 'UNCX01', 'machine_number': '1'}},
+    {'geometry_type': {'geometry_type': 'OUTPUT_ON_CENTER'}},
+    {'units': {'units': 'INCHES'}},
+    {'tool': {'tool': '1'}},
+    {'tool_size': {'tool_size': '0.500000'}},
+    {'coordinate_system': {'coordinate_system': 'xyz'}},
+    {'spindle_speed': {'control': 'RPM,', 'speed': 1000.0, 'direction': 'CLW'}},
+    {'coolant': {'bool': True}},
+    {'max_speed': {'bool': True}},
+    {'move': {'x': -0.35, 'y': 5.0107142857, 'z': 1.225}},
+    {'speed': {'speed': 10.0}},
+    {'move': {'x': -0.35, 'y': 5.0107142857, 'z': 1.1}},
+    {'move': {'x': 3.35, 'y': 5.0107142857, 'z': 1.1}},
+    {'move': {'x': 3.35, 'y': 5.0107142857, 'z': 1.2}},
+    {'spindle_speed': {'control': 'OFF,'}}
+   ]
+
+
+def _print(results):
+    for result in results:
+        print(result)
+
+
+class TestnScryptConverter(unittest.TestCase):
+    def setUp(self):
+        self.nScryptConverter = NscryptConverter()
+
+    def test_nScrypt_constructor(self):
+        self.assertIsNotNone(self.nScryptConverter)
+        self.assertIsNotNone(self.nScryptConverter._units)
+
+    def test_invalid_command(self):
+        # Arrange
+        INVALID_COMMAND = {
+            "INVALID": {}
+        }
+        exp_result = [VERSION,
+                      TYPE,
+                      "!INVALID COMMAND: {'INVALID': {}}",
+        ]
+        # Act
+        result = self.nScryptConverter.translate([INVALID_COMMAND])
+        # Assert
+        self.assertEqual(result, exp_result)
+
+    def test_move_command(self):
+        # Arrange
+        MOVE_COMMAND = [
+            {"move": {"x": "10.0", "y": "20.0", "z": "30.0"}}
+        ]
+        exp_result = [VERSION,
+                      TYPE,
+                      "10.0 20.0 30.0 0.0 0.0",
+        ]
+        # Act
+        result = self.nScryptConverter.translate(MOVE_COMMAND)
+        # Assert
+        self.assertEqual(result, exp_result)
+
+    def test_unit_command(self):
+        # Arrange
+        MOVE_COMMAND = [
+            {"units": {"units": "INCHES"}}
+        ]
+        exp_result = [VERSION,
+                      TYPE,
+        ]
+        exp_units = "INCHES"
+        # Act
+        result = self.nScryptConverter.translate(MOVE_COMMAND)
+        units = self.nScryptConverter._units
+        # Assert
+        self.assertEqual(result, exp_result)
+        self.assertEqual(units, exp_units)
+        
+    def test_unit_move_command(self):
+        # Arrange
+        MOVE_COMMAND = [
+            {"units": {"units": "INCHES"}},
+            {"move": {"x": "10.0", "y": "20.0", "z": "30.0"}}
+        ]
+        exp_result = [VERSION,
+                      TYPE,
+                      "254.0 508.0 762.0 0.0 0.0",
+        ]
+        exp_units = "INCHES"
+        # Act
+        result = self.nScryptConverter.translate(MOVE_COMMAND)
+        units = self.nScryptConverter._units
+        # Assert
+        self.assertEqual(result, exp_result)
+        self.assertEqual(units, exp_units)
+        
+    def test_initial_output(self):
+        # Arrange
+        exp_result = [VERSION,
+                      TYPE,
+        ]
+        # Act
+        result = self.nScryptConverter.translate([])
+        # Assert
+        self.assertEqual(result, exp_result)
+
+    def test_tool_on_off_command(self):
+        # Arrange
+        TOOL_ON_OFF_COMMAND = [
+            {"spindle_speed": {"control":"RPM"}},
+            {"spindle_speed": {"control": "OFF"}}
+        ]
+        exp_result = [VERSION,
+                      TYPE,
+                      "TOOL ON",
+                      "TOOL OFF"
+        ]
+        # Act
+        result = self.nScryptConverter.translate(TOOL_ON_OFF_COMMAND)
+        # Assert
+        self.assertEqual(result, exp_result)
+    
+    def test_sample_output(self):
+        # Arrange
+        exp_result = [VERSION,
+                      TYPE,
+                      "!INVALID COMMAND: {'title': {'type': 'Pro/CLfile', 'version': '11.0 - 11.0.0.0'}}",
+                      "!INVALID COMMAND: {'manufacturer_number': {'manufacturer_number': 'MFG0006'}}",
+                      "!INVALID COMMAND: {'part_number': {'part_number': 'MFG0006'}}",
+                      "!INVALID COMMAND: {'feature_number': {'feature_number': '402'}}",
+                      "!INVALID COMMAND: {'machine_info': {'machine_type': 'UNCX01', 'machine_number': '1'}}",
+                      "!INVALID COMMAND: {'geometry_type': {'geometry_type': 'OUTPUT_ON_CENTER'}}",
+                      "!INVALID COMMAND: {'tool': {'tool': '1'}}",
+                      "!INVALID COMMAND: {'tool_size': {'tool_size': '0.500000'}}",
+                      "!INVALID COMMAND: {'coordinate_system': {'coordinate_system': 'xyz'}}",
+                      "TOOL ON",
+                      "!INVALID COMMAND: {'coolant': {'bool': True}}",
+                      "!INVALID COMMAND: {'max_speed': {'bool': True}}",
+                      "-8.889999999999999 127.27214285678 31.115000000000002 0.0 0.0",
+                      "!INVALID COMMAND: {'speed': {'speed': 10.0}}",
+                      "-8.889999999999999 127.27214285678 27.94 0.0 0.0",
+                      "85.09 127.27214285678 27.94 0.0 0.0",
+                      "85.09 127.27214285678 30.479999999999997 0.0 0.0",
+                      "TOOL OFF"
+        ]
+        # Act
+        result = self.nScryptConverter.translate(PARSED_COMMANDS)
+        # Assert
+        self.assertEqual(result, exp_result)
+
+if __name__ == "__main__":
+    unittest.main()
