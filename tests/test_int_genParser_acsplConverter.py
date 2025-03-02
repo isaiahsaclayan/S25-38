b63c248af30332825c49339ebdb7409b6030cb80
Isaiah Amir Saclayan <isaiahsaclayan@vt.edu>
2025-03-02 14:00:45 -0500

    organized docstrings

--- tests/test_int_genParser_acsplConverter.py
+++ tests/test_int_genParser_acsplConverter.py
@@ -10,12 +10,16 @@ from parser import GenericParser
 
 class TestIntegrationParserToACSPL(unittest.TestCase):
     def setUp(self):
-        """ Set up the test environment. """
+        """
+        Set up the test environment.
+        """
         self.acsplConverter = AcsplConverter()
         self.genericParser = GenericParser("../tests/resources/op010.ncl.1")
 
     def test_conversion_to_translate_noException(self):
-        """ Ensure parsing and conversion do not raise any exceptions. """
+        """
+        Ensure parsing and conversion do not raise any exceptions.
+        """
         # Arrange
         parsed_commands = self.genericParser.parse_commands() # Parse the file and get the parsed commands
 
@@ -60,6 +64,9 @@ class TestIntegrationParserToACSPL(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open, read_data="")
     def test_empty_input_file(self, mock_file):
+        """
+        Ensure an empty file does not raise any exceptions and does not get processed.
+        """
         # Arrange, use the mock to simulate an empty file
         mock_parser = GenericParser("empty_file.ncl.1")
 
@@ -73,6 +80,9 @@ class TestIntegrationParserToACSPL(unittest.TestCase):
 
     @patch("builtins.open", new_callable=mock_open, read_data="INVALIDCOMMAND / 1.0, 2.0, 3.0")
     def test_invalid_command(self, mock_file):
+        """
+        Ensure an invalid command does not raise any exceptions and does not get processed.
+        """
         # Arrange, use the mock to simulate an invalid command
         mock_parser = GenericParser("invalid_command.ncl.1")
 
@@ -84,7 +94,6 @@ class TestIntegrationParserToACSPL(unittest.TestCase):
         self.assertEqual(len(parsed_commands), 0) # Ensure the command was not processed by parser
         self.assertEqual(len(translated_command), 0) # Ensure the command was not translated by converter
         self.assertIn("INVALIDCOMMAND", mock_parser.unparsedCommands[0]) # Ensure the invalid command is in the unparsed list
-
 
     # TODO remove this test before delivery
     def test_print(self):
