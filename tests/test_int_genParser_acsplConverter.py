650e1f3982c5695f639f90f68de07d7821ca14a1
Isaiah Amir Saclayan <isaiahsaclayan@vt.edu>
2025-03-02 13:17:33 -0500

    added type check integration test

--- tests/test_int_genParser_acsplConverter.py
+++ tests/test_int_genParser_acsplConverter.py
@@ -6,10 +6,12 @@ from parser import GenericParser
 
 class TestIntegrationParserToACSPL(unittest.TestCase):
     def setUp(self):
+        """ Set up the test environment. """
         self.acsplConverter = AcsplConverter()
         self.genericParser = GenericParser("../tests/resources/op010.ncl.1")
 
     def test_conversion_to_translate_noException(self):
+        """ Ensure parsing and conversion do not raise any exceptions. """
         # Arrange
         parsed_commands = self.genericParser.parse_commands() # Parse the file and get the parsed commands
 
@@ -20,6 +22,10 @@ class TestIntegrationParserToACSPL(unittest.TestCase):
             self.fail(f"Exception raised during translation: {e}")
 
     def test_conversion_to_translation_noInvalidCommands(self):
+        """
+        Ensure translated output does not contain invalid commands, as all in the current
+        Creo file are supported.
+        """
         # Arrange
         parsed_commands = self.genericParser.parse_commands()
 
@@ -29,6 +35,23 @@ class TestIntegrationParserToACSPL(unittest.TestCase):
         # Assert
         for command in translated_commands:
             self.assertNotIn("INVALID", command)
+
+    def test_conversion_to_translation_typeCheck(self):
+        """
+        Ensure the translated output is a list of strings.
+        """
+        # Arrange
+        parsed_commands = self.genericParser.parse_commands()
+
+        # Act
+        translated_commands = self.acsplConverter.translate(parsed_commands)
+
+        # Assert
+        self.assertIsInstance(translated_commands, list) # Ensure the output is a list
+        self.assertGreater(len(translated_commands), 0) # Ensure there are commands
+        # Ensure all commands are strings
+        for command in translated_commands:
+            self.assertIsInstance(command, str)
 
     # TODO remove this test before delivery
     def test_print(self):
