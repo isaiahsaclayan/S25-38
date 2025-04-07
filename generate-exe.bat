a2ee2ff17833e7019a2b9e55bff5ab9ed0f068ab
Isaiah Amir Saclayan <isaiahsaclayan@vt.edu>
2025-04-06 20:21:11 -0400

    added comments to batch file

--- generate-exe.bat
+++ generate-exe.bat
@@ -1,8 +1,11 @@
+:: Description: This batch script automates the process of generating an executable file from a Python script using PyInstaller.
+
+:: Remove any existing build directory
 if exist build (
     rmdir /s /q build
 )
 
-pyinstaller --clean build/transpiler
+:: Generate the executable
 pyinstaller --name transpiler ^
     --onefile ^
     --windowed ^
